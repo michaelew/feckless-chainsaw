78a1c7548d6039733a6dab26a6f1e42d5df0b7ed
Michael Walker <321homerepair@gmail.com>
2015-09-06 20:51:15 -0400

    Initial commit.

---
+++ routes/contact.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET about page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Agency' });
+});
+
+router.post('/send', function(req, res, next){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'michael@verus.io',
+      pass: 'tablets321'
+    }
+  });
+  var mainOptions = {
+    from: 'Michael Walker <michael@verus.io>',
+    to: 'admin@idedicated.net',
+    subject: 'Website Submission',
+    text: 'You have a new submission with the following details...Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.message+ '',
+    html: '<p>You have a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+  };
+  transporter.sendMail(mainOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.redirect('/');
+    } else {
+      console.log('Message Sent: '+info.response);
+      res.redirect('/');
+    }
+  });
+});
+
+module.exports = router;
