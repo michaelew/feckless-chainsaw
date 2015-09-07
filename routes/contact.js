41b625e53f235dc7853a16a181da4dddbfa84638
Michael Walker <321homerepair@gmail.com>
2015-09-06 21:42:30 -0400

    Initial commit.

--- routes/contact.js
+++ routes/contact.js
@@ -11,13 +11,13 @@ router.post('/send', function(req, res, next){
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'michael@verus.io',
-      pass: 'tablets321'
+      user: 'your@gmail.com',
+      pass: 'yourpassword'
     }
   });
   var mainOptions = {
-    from: 'Michael Walker <michael@verus.io>',
-    to: 'admin@idedicated.net',
+    from: 'Michael Walker <your@gmail.com>',
+    to: 'recipient@email.com',
     subject: 'Website Submission',
     text: 'You have a new submission with the following details...Name: '+req.body.name+ ' Email: '+req.body.email+ ' Message: '+req.body.message+ '',
     html: '<p>You have a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
